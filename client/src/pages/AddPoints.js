67bb20b4996094af610678233ff54ad593d87932
Ha Phan <haphan64@outlook.com>
2020-09-03 22:30:27 -0700

    change font, center

--- client/src/pages/AddPoints.js
+++ client/src/pages/AddPoints.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Container, Form, Button, Heading, Table } from 'react-bulma-components';
-import 'react-bulma-components/dist/react-bulma-components.min.css';
+import { Columns, Container, Form, Button, Heading, Table } from 'react-bulma-components';
+import "./style.sass";
 
 function AddPoints () {
 
@@ -8,66 +8,66 @@ function AddPoints () {
        
     return (
             
-        <Container>  
-            <Heading>Add Points</Heading>
+        <Container className="is-mobile">
+            <Container className="is-centered">
+                <Columns.Column className="is-narrow has-text-centered ">
 
-            <Heading subtitle size={5}>Current Balance: {50}</Heading>
+                    <Heading className="heading1">Add Points</Heading>
 
-            <Container style={{ marginBottom: 40 }}>
-                <Heading subtitle size={4}>
-                    Select Activity to Add
-                </Heading>               
-                    <Table className="is-narrow is-hoverable is-bordered">
-                        <thead>
-                            <tr>                        
-                                <th>Activity</th>                        
-                                <th>Point</th>
-                                <th>Select</th>                                
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th><img style={{ width: 40 }} src="https://cdn2.iconfinder.com/data/icons/new-year-resolutions/64/resolutions-05-128.png"></img>Reading</th>                                
-                                <td>20</td>
-                                <td>X</td>                                
-                            </tr>
-                        </tbody>
+                    <Heading subtitle size={5}>Current Balance: {50}</Heading>
 
-                    </Table>
-                
+                    <Container style={{ marginBottom: 40 }}>
+                        <Heading subtitle size={5} className="heading2">
+                            Select Activity to Add
+                        </Heading>               
+                            <Table className="is-narrow is-hoverable is-bordered">
+                                <thead>
+                                    <tr>                        
+                                        <th>Activity</th>                        
+                                        <th>Point</th>
+                                        <th>Select</th>                                
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th><img style={{ width: 40 }} src="https://cdn2.iconfinder.com/data/icons/new-year-resolutions/64/resolutions-05-128.png"></img>Reading</th>                                
+                                        <td>20</td>
+                                        <td>X</td>                                
+                                    </tr>
+                                </tbody>
+
+                            </Table>
+                        
+                    </Container>
+                    
+                    {/* <Container>
+                        <Heading subtitle size={4}>
+                            Add New Activity
+                        </Heading>
+
+                        <form onSubmit>
+                            <Field>                
+                                <Control>                    
+                                    <input className="input" type="text" name="Activities" placeholder="Enter Activity"/>                                                
+                                </Control> 
+                                <Control>                    
+                                    <input className="input" type="text" name="point" placeholder="Enter point for Activity"/>                                                
+                                </Control>               
+                            </Field>
+                        </form> 
+                        
+                    </Container> */}
+
+                    <Container style={{ marginTop: 40 }}>
+                        <Field>                            
+                            <Button className="is-primary is-rounded" >Add Points</Button>                            
+                        </Field> 
+                        <Field>                            
+                            <Button className="is-primary is-rounded" ><a className="has-text-white" href="/create-activities">Create Activities</a></Button>                            
+                        </Field>               
+                    </Container>
+                </Columns.Column>
             </Container>
-            
-            {/* <Container>
-                <Heading subtitle size={4}>
-                    Add New Activity
-                </Heading>
-
-                <form onSubmit>
-                    <Field>                
-                        <Control>                    
-                            <input className="input" type="text" name="Activities" placeholder="Enter Activity"/>                                                
-                        </Control> 
-                        <Control>                    
-                            <input className="input" type="text" name="point" placeholder="Enter point for Activity"/>                                                
-                        </Control>               
-                    </Field>
-                </form> 
-                
-            </Container> */}
-
-            <Container style={{ marginTop: 40 }}>
-                <Field>
-                    <Control>
-                        <Button className="is-success">Add Points</Button>
-                    </Control>
-                </Field> 
-                <Field>
-                    <Control>
-                        <Button className="is-link is-light"><a href="/create-activities">Create Activities</a></Button>
-                    </Control>
-                </Field>               
-            </Container>
-
         </Container>
             
     )
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,6 +1,6 @@
 import React, {useRef} from "react";
-import { Container, Form, Button, Heading } from 'react-bulma-components';
-import 'react-bulma-components/dist/react-bulma-components.min.css';
+import { Columns, Container, Form, Button, Heading } from 'react-bulma-components';
+import "./style.sass";
 
 import api from "../utils/api";
 import { useLogin } from "../utils/auth";
@@ -32,53 +32,55 @@ function Register() {
     const { Input, Field, Control, Label } = Form;
     
     return (
-        <Container>
-            <Heading>Register</Heading>
-            <form onSubmit={handleSubmit}>
-            <Field>                
-                <Control className="has-icons-left has-icons-right">                    
-                    <input className="input" type="text" name="name" placeholder="username" ref={nameInput} />
-                    <span className="icon is-small is-left">
-                    <i className="fas fa-user"></i>
-                    </span>                    
-                </Control>                
-            </Field>            
-            <br />
-            <Field>
-                <Control className="has-icons-left has-icons-right">                                    
-                    <input className="input" type="email" name="email" placeholder="email" ref={emailInput} />
-                    <span className="icon is-small is-left">
-                        <i className="fas fa-envelope"></i>
-                    </span>                    
-                </Control>
-            </Field>             
-            <br />
-            <Field>
-                <Control className="has-icons-left">                                 
-                    <input className="input" type="password" name="password" placeholder="Password" ref={passwordInput} />
-                    <span className="icon is-small is-left">
-                        <i className="fas fa-lock"></i>
-                    </span>                            
-                </Control>
-            </Field>            
-            <br />
-            <Field>
-                <Control className="has-icons-left">                                 
-                    <input className="input" type="password" name="password2" placeholder="Retype password" ref={password2Input} />                    
-                    <span className="icon is-small is-left">
-                        <i className="fas fa-lock"></i>
-                    </span>                            
-                </Control>
-            </Field>            
-            <br />
-            <Field>
-                <Control>
-                    <Button className="is-success">Submit</Button>
-                </Control>
-            </Field>           
-            </form>
-
+        <Container className="is-mobile">
+            <Container className="is-centered">
+                <Columns.Column className="is-narrow has-text-centered">
+                    <Heading className="heading1">Register</Heading>
+                    <form onSubmit={handleSubmit}>
+                    <Field className="is-narrow">                
+                        <Control className="has-icons-left has-icons-right">                    
+                            <input className="input" type="text" name="name" placeholder="username" ref={nameInput} />
+                            <span className="icon is-small is-left">
+                            <i className="fas fa-user"></i>
+                            </span>                    
+                        </Control>                
+                    </Field>            
+                    <br />
+                    <Field>
+                        <Control className="has-icons-left has-icons-right">                                    
+                            <input className="input" type="email" name="email" placeholder="email" ref={emailInput} />
+                            <span className="icon is-small is-left">
+                                <i className="fas fa-envelope"></i>
+                            </span>                    
+                        </Control>
+                    </Field>             
+                    <br />
+                    <Field>
+                        <Control className="has-icons-left">                                 
+                            <input className="input" type="password" name="password" placeholder="Password" ref={passwordInput} />
+                            <span className="icon is-small is-left">
+                                <i className="fas fa-lock"></i>
+                            </span>                            
+                        </Control>
+                    </Field>            
+                    <br />
+                    <Field>
+                        <Control className="has-icons-left">                                 
+                            <input className="input" type="password" name="password2" placeholder="Retype password" ref={password2Input} />                    
+                            <span className="icon is-small is-left">
+                                <i className="fas fa-lock"></i>
+                            </span>                            
+                        </Control>
+                    </Field>            
+                    <br />
+                    <Field>                        
+                        <Button className="is-primary is-rounded">Submit</Button>                        
+                    </Field>           
+                    </form>
+                </Columns.Column> 
+            </Container> 
         </Container>
+        
     )
 }
 
