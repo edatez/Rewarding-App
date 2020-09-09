39260c623cdc6db289e246a6fea27cbf7fb866fd
Aidan Q <aidanq52@gmail.com>
2020-09-08 20:53:53 -0700

    updating the children controller along with reward controller

--- models/User.js
+++ models/User.js
@@ -1,5 +1,21 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+// require("ChildrenSchema")
+
+const rewardsSubSchema = mongoose.Schema({
+  rewardName: {
+      type: String,
+      required: true
+  },
+  rewardPoints:{
+      type: Number,
+      required: true
+  },
+  redeemed:{
+      type: Boolean,
+      default: false
+  }
+})
 
 // Create Schema
 const UserSchema = new Schema({
@@ -19,10 +35,19 @@ const UserSchema = new Schema({
     type: Date,
     default: Date.now
   },
-  children:[{
-    type: Schema.Types.ObjectId,
-    ref: "Children"
-  }],
+
+  children: [new mongoose.Schema({
+    childName:{
+      type: String,
+      required: true
+    },
+    pointsEarned:[{
+        type: Number,
+        required: true
+    }],
+    rewards:[rewardsSubSchema]
+  })],
+
   activities:[{
     type: Schema.Types.ObjectId,
     ref: "Activity"
