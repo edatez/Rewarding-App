7e86fa070b7a157d460867b422289e148a852de4
Eda Tuncer <edatezcanli@gmail.com>
2020-09-12 12:50:56 -0700

    links are added to the settings page and after register submit directs to add children page

--- client/src/pages/Settings.js
+++ client/src/pages/Settings.js
@@ -1,18 +1,29 @@
 import React from "react";
-import { Content } from "react-bulma-components";
+import { Content, Button } from "react-bulma-components";
 
 
 function Settings() {
   return (
-
-    
-
+ 
      <Content className="has-text-centered"> 
       <p>
      
         <a href="/login">Start Your Motivation!</a>. .
       </p>
- </Content> 
+      <Button className="is-primary is-rounded"><a className="has-text-white" href="/add-children">Add Children</a></Button>
+      <br></br>
+      <br></br>
+      <Button className="is-primary is-rounded"><a className="has-text-white" href="/create-rewards">Create Rewards</a></Button>
+      <br></br>
+      <br></br>
+      <Button className="is-primary is-rounded"><a className="has-text-white" href="/create-activities">Create Activities</a></Button>
+      <br></br>
+      <br></br>
+      <Button className="is-primary is-rounded" ><a className="has-text-white" href="/redeem-rewards">Redeem Rewards</a></Button>
+      <br></br>
+      <br></br>
+      <Button className="is-primary is-rounded"><a className="has-text-white" href="/dashboard">Dashboard</a></Button>
+    </Content> 
 
   );
 }
